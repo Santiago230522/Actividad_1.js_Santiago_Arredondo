aab6f7071c805cdb2c3a8aa619062642600ca035
unknown <santyrhiios17@gmail.com>
2023-08-01 19:36:09 -0500

    Mi primer commit

---
+++ Prueba.js
@@ -0,0 +1,7 @@
+const Name = "Santiago"
+const last_name = "Arredondo Rios"
+const Birthdate = "25/04/1999"
+const City = "Medellin, Ant"
+
+console.log("Hola, mi nombre es ", Name ,  last_name , "naci el ", Birthdate , 
+"en la cuidad de ", City );
